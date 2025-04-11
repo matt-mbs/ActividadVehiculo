861dd8f9928978635db574156b971e438777f4a5
Matt <Matt@DESKTOP-NQ24F58>
2025-04-11 10:38:59 -0500

    Add project files.

---
+++ Vehiculo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehiculo;
+
+namespace ActividadVehiculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi("ABC123", "Toyota Prius", 120);
+            taxi.setNumeroLicencia("LIC-456789");
+
+            Autobus autobus = new Autobus("XYZ789", "Mercedes Sprinter", 180);
+            autobus.setNumeroPlazas(30);
+
+            Console.WriteLine("TAXI:");
+            Console.WriteLine("Matrícula: " + taxi.getMatricula());
+            Console.WriteLine("Modelo: " + taxi.getModelo());
+            Console.WriteLine("Potencia: " + taxi.getPotenciaCV() + " CV");
+            Console.WriteLine("Número de Licencia: " + taxi.getNumeroLicencia());
+
+            Console.WriteLine("\nAUTOBÚS:");
+            Console.WriteLine("Matrícula: " + autobus.getMatricula());
+            Console.WriteLine("Modelo: " + autobus.getModelo());
+            Console.WriteLine("Potencia: " + autobus.getPotenciaCV() + " CV");
+            Console.WriteLine("Número de Plazas: " + autobus.getNumeroPlazas());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Vehiculo/Vehiculo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehiculo
+{
+    public class Vehiculo
+    {
+        private string matricula;
+        private string modelo;
+        private int potenciaCV;
+
+        public Vehiculo(string matricula, string modelo, int potenciaCV)
+        {
+            this.matricula = matricula;
+            this.modelo = modelo;
+            this.potenciaCV = potenciaCV;
+        }
+
+        public string getMatricula()
+        {
+            return matricula;
+        }
+
+        public string getModelo()
+        {
+            return modelo;
+        }
+
+        public int getPotenciaCV()
+        {
+            return potenciaCV;
+        }
+    }
+    public class Taxi : Vehiculo
+    {
+        private string numeroLicencia;
+
+        public Taxi(string matricula, string modelo, int potenciaCV)
+            : base(matricula, modelo, potenciaCV)
+        {
+        }
+
+        public void setNumeroLicencia(string numeroLicencia)
+        {
+            this.numeroLicencia = numeroLicencia;
+        }
+
+        public string getNumeroLicencia()
+        {
+            return numeroLicencia;
+        }
+    }
+
+   
+    public class Autobus : Vehiculo
+    {
+        private int numeroPlazas;
+
+        public Autobus(string matricula, string modelo, int potenciaCV)
+            : base(matricula, modelo, potenciaCV)
+        {
+        }
+
+        public void setNumeroPlazas(int numeroPlazas)
+        {
+            this.numeroPlazas = numeroPlazas;
+        }
+
+        public int getNumeroPlazas()
+        {
+            return numeroPlazas;
+        }
+    }
+}
